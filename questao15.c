7f8a4fbf7287291c559a2a6839f4b04ca85c32b9
carlosbarros007 <carlosbarros723513@gmail.com>
2023-03-10 20:39:55 -0300

    organizando melhoer os arquivos

--- questao15.c
+++ questao15.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <conio.h>
+#include <curses.h>
 
 /*.Elabore um programa que realize 5 perguntas para uma pessoa sobre um determinado 
 crime. As perguntas são:
@@ -18,7 +18,7 @@ classificado como "Inocente".*/
 // diz se o sujeito e culpado ou inocente
 float culpadoOuInocente(float resposta1, float resposta2, float resposta3, float resposta4, float resposta5){
 
-   float suspeito = resposta1 + resposta2 + resposta3 + resposta4 + resposta5 - 1; // o menos um esta aqui para corrigir um bug, ou seja ele e um ajuste tecnico.
+   float suspeito = resposta1 + resposta2 + resposta3 + resposta4 + resposta5; // o menos um esta aqui para corrigir um bug, ou seja ele e um ajuste tecnico.
 
    if(suspeito == 5){
 
